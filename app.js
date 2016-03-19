e5b48cb07769a2f645bb2637a601d3aa22f8f944
Nick Brennan <nick_p_b@yahoo.com>
2016-03-19 01:18:41 -0700

    can drop a piece

--- app.js
+++ app.js
@@ -10,7 +10,35 @@ $(function(){
     
     //get index of column where piece was dropped
     $('.moveCell').click(function(){
-            console.log($(this).attr('value'));
-        }
-    );
+            var col = $(this).attr('value');
+            console.log(col);
+            if(boardArr[0][col] !== '.'){
+                return;
+            }else if(boardArr[5][col] === '.'){
+                boardArr[5][col] = 'r';
+            }
+        renderBoard();
+    });
 });
+
+var boardArr = [
+    ['.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.']
+];
+
+function renderBoard(){
+    boardArr.forEach(function(row, ind1){
+        row.forEach(function (val, ind2){
+            console.log(boardArr[ind1][ind2]);
+            var idString = '#cell' + ind1.toString() + ind2.toString();
+            if(val === 'r'){
+                console.log($(idString));
+                $(idString).css('background-color', 'red');
+            }
+        });
+    });
+}
